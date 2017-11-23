6c8671bb45974ae3e2cb61d9ed3aac6f40d100d1
MohamedDer <medder12@gmail.com>
2017-11-23 21:21:52 +0000

    Code improvements -v2.0- :  - Naming changes  - Access to members updated  - few more improvements

--- app/src/main/java/derkaoui/coms/facebook/myphotos/AlbumsAdapter.java
+++ app/src/main/java/derkaoui/coms/facebook/myphotos/AlbumsAdapter.java
@@ -15,30 +15,29 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-/**
- * Created by Mohamed Derkaoui on 25/05/2017.
- */
 
 public class AlbumsAdapter extends BaseAdapter {
 
-    private static LayoutInflater inflater = null;
-    String [] names;
-    Context context;
-    String[] defaultImageUrl;
-    ArrayList<Album> Albums;
+    private static final int DEFAULT_IMAGE_WIDTH = 410;
+    private static final int DEFAULT_IMAGE_HEIGHT = 450;
+    private LayoutInflater inflater = null;
+    private String[] names;
+    private Context context;
+    private String[] defaultImageUrl;
+    private ArrayList<Album> Albums;
 
 
-    public AlbumsAdapter(AlbumsActivity mainActivity, ArrayList<Album> myAlbums) {
+    AlbumsAdapter(AlbumsActivity mainActivity, ArrayList<Album> myAlbums) {
 
-            context=mainActivity;
+        this.context = mainActivity.getApplicationContext();
         this.Albums = myAlbums;
 
         this.names = new String[myAlbums.size()];
         this.defaultImageUrl = new String[myAlbums.size()];
 
         for (int i = 0; i < myAlbums.size(); i++) {
-            names[i] = myAlbums.get(i).name_Alb;
-            defaultImageUrl[i] = myAlbums.get(i).photosUrl.get(0);
+            names[i] = myAlbums.get(i).albumName;
+            defaultImageUrl[i] = myAlbums.get(i).photosUrls.get(0);
         }
             inflater = ( LayoutInflater )context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
@@ -46,25 +45,21 @@ public class AlbumsAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
-            // TODO Auto-generated method stub
-            return names.length;
+            return names == null ? 0 : names.length;
         }
 
         @Override
         public Object getItem(int position) {
-            // TODO Auto-generated method stub
             return position;
         }
 
         @Override
         public long getItemId(int position) {
-            // TODO Auto-generated method stub
             return position;
         }
 
         @Override
         public View getView(final int position, View convertView, ViewGroup parent) {
-            // TODO Auto-generated method stub
             Holder holder=new Holder();
             View rowView;
 
@@ -73,17 +68,16 @@ public class AlbumsAdapter extends BaseAdapter {
             holder.alb_img =(ImageView) rowView.findViewById(R.id.albimage);
 
             holder.alb_name.setText(names[position]);
-            Picasso.with(context).load(defaultImageUrl[position]).resize(410, 450).into(holder.alb_img);
+            Picasso.with(context).load(defaultImageUrl[position]).resize(DEFAULT_IMAGE_WIDTH, DEFAULT_IMAGE_HEIGHT).into(holder.alb_img);
 
             rowView.setOnClickListener(new OnClickListener() {
 
                 @Override
                 public void onClick(View v) {
-                    // TODO Auto-generated method stub
                     Toast.makeText(context, "You selected "+names[position], Toast.LENGTH_SHORT).show();
                     Intent viewimages =  new Intent(context, ImagesActivity.class);
-                    viewimages.putExtra("album_name", Albums.get(position).name_Alb);
-                    viewimages.putExtra("photos_urls", Albums.get(position).photosUrl);
+                    viewimages.putExtra("album_name", Albums.get(position).albumName);
+                    viewimages.putExtra("photos_urls", Albums.get(position).photosUrls);
 
                     context.startActivity(viewimages);
 
--- app/src/main/java/derkaoui/coms/facebook/myphotos/ImagesAdapter.java
+++ app/src/main/java/derkaoui/coms/facebook/myphotos/ImagesAdapter.java
@@ -14,16 +14,15 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-/**
- * Created by Mohamed Derkaoui on 25/05/2017.
- */
 
 public class ImagesAdapter extends BaseAdapter {
 
 
-    private static LayoutInflater inflater=null;
+    private final int DEFAULT_IMAGE_WIDTH = 410;
+    private final int DEFAULT_IMAGE_HEIGHT = 450;
+    LayoutInflater inflater = null;
     Context context;
-    ArrayList<String> imagesUrls;
+    private ArrayList<String> imagesUrls;
 
     public ImagesAdapter(ImagesActivity mainActivity, ArrayList<String> imagesurls) {
         // TODO Auto-generated constructor stub
@@ -47,13 +46,11 @@ public class ImagesAdapter extends BaseAdapter {
 
     @Override
     public long getItemId(int position) {
-        // TODO Auto-generated method stub
         return position;
     }
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        // TODO Auto-generated method stub
         final Holder holder=new Holder();
         View rowView;
 
@@ -61,13 +58,12 @@ public class ImagesAdapter extends BaseAdapter {
         holder.chk_img =(CheckBox) rowView.findViewById(R.id.chekcboxshw);
         holder.alb_img =(ImageView) rowView.findViewById(R.id.imageshw);
 
-        Picasso.with(context).load(imagesUrls.get(position)).resize(410, 450).into(holder.alb_img);
+        Picasso.with(context).load(imagesUrls.get(position)).resize(DEFAULT_IMAGE_WIDTH, DEFAULT_IMAGE_HEIGHT).into(holder.alb_img);
 
         rowView.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
 
                 if (holder.chk_img.isChecked()){
                     Toast.makeText(context, "Image unselected ! ", Toast.LENGTH_SHORT).show();
